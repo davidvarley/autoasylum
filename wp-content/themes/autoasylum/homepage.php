f7df3a2e51df13302bfe2d3f88870c83dddfeacf
dave_varley <thedavidvarley@gmail.com>
2016-04-27 17:10:20 +0100

    27th April logoff

---
+++ wp-content/themes/autoasylum/homepage.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template Name: HomePage
+ *
+ * @package WordPress
+ * @subpackage autoasylum
+ * @since 2016
+ */
+
+
+get_header(); ?>
+
+	<div class="jumbotron">
+
+		<img src="wp-content/themes/autoasylum/img/jumbotron.jpg">
+
+			<div class="container">
+
+			</div>
+
+	</div>
+
+
+
+
+
+
+
+<?php
+
+get_footer(); ?>
